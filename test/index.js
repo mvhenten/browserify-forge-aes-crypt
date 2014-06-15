fd1a4a83c00001033fbeb7f1241b49c294d26b3c
Matthijs van Henten <matthijs@ischen.nl>
2014-06-15 23:35:41 +0200

    adding test

--- test/index.js
+++ test/index.js
@@ -1,15 +1,17 @@
+'use strict';
+
 var test = require('tape'),
     forge = require('../index');
 
 var Demo = {
-    encrypt: function( password, data ) {
+    encrypt: function(password, data) {
         var salt = forge.random.getBytesSync(128),
             iv = forge.random.getBytesSync(16),
             key = forge.pbkdf2(password, salt, 100, 16),
             cipher = forge.aes.createEncryptionCipher(key, 'CBC');
 
         cipher.start(iv);
-        cipher.update(forge.util.createBuffer( data ), 'utf8');
+        cipher.update(forge.util.createBuffer(data), 'utf8');
         cipher.finish();
 
         return {
@@ -30,15 +32,15 @@ var Demo = {
 
         return cipher.output;
     },
-}
+};
 
-test('Should be able to encrypt and decrypt', function(assert){
+test('Should be able to encrypt and decrypt', function(assert) {
     var password = 'SECRET',
-        secret  = 'Lorem ipsum sit amet';
+        secret = 'Lorem ipsum sit amet';
 
-    var crypted = Demo.encrypt( password, secret );
-    var decrypted = Demo.decrypt( password, crypted.secret, crypted.salt, crypted.iv );
+    var crypted = Demo.encrypt(password, secret);
+    var decrypted = Demo.decrypt(password, crypted.secret, crypted.salt, crypted.iv);
 
-    assert.equal( decrypted.data, secret );
+    assert.equal(decrypted.data, secret);
     assert.end();
 });
