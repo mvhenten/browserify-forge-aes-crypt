22509a3def6db73838ca6b3565e06cb9d7aac7ac
Matthijs van Henten <matthijs@ischen.nl>
2014-06-16 20:34:54 +0200

    additional heuristics to fix left-side assignemnts

--- bin/import.js
+++ bin/import.js
@@ -14,7 +14,7 @@ var MODULE_NAME = path.basename(TARGET).replace(path.extname(TARGET), '');
 var src = fs.readFileSync(SOURCE);
 var obj = esp.parse(src.toString());
 
-function genExpression(name) {
+function mkRequire(name) {
     return {
         type: 'CallExpression',
         callee: {
@@ -29,25 +29,61 @@ function genExpression(name) {
     };
 }
 
+function mkVar(obj, name, right) {
+    obj.type = 'VariableDeclaration';
+    obj.kind = 'var';
+    obj.declarations = [{
+        type: 'VariableDeclarator',
+        id: {
+            type: 'Identifier',
+            name: name
+        },
+        init: right
+    }];
+}
+
+function mkModuleExports(value) {
+    value.object.name = 'module';
+    value.property.name = 'exports';
+}
+
 function transform(obj, moduleName) {
     _.each(obj, function(value, key) {
         if (typeof value !== 'object' || !value) return;
+
+        if (value.type == 'AssignmentExpression') {
+
+            if (value.left.type == 'MemberExpression' && value.left.object.name == 'forge') {
+                if (value.left.property.name !== moduleName) {
+                    mkVar(obj, value.left.property.name, value.right);
+                    transform(value.right, moduleName);
+                    return;
+                }
+            }
+
+            if (value.left.type == 'MemberExpression' && value.left.object.object && value.left.object.object.name == 'forge') {
+                var name = value.left.object.property.name;
+
+                value.left.object = {
+                    type: 'Identifier',
+                    name: name == moduleName ? 'module.exports' : name
+                };
+
+                transform(value.right, moduleName);
+                return;
+            }
+        }
 
         if (value.type !== 'MemberExpression' || (value.object && value.object.name !== 'forge'))
             return transform(value, moduleName);
 
         if (value.property.type === 'Identifier') {
             if (key == 'left' || key == 'object') {
-                if (value.property.name == moduleName) {
-
-                    value.object.name = 'module';
-                    value.property.name = 'exports';
-                    return;
-
-                }
+                if (value.property.name == moduleName)
+                    return mkModuleExports(value);
             }
 
-            obj[key] = genExpression(value.property.name);
+            obj[key] = mkRequire(value.property.name);
         }
     });
 }
